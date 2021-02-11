7e1d24b9b44badf1c90dfc8b01b1b698f0327bb3
emkat93 <eallen5064@students.mchenry.edu>
2021-02-11 13:14:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/thepinchgesture/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.thepinchgesture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.widget.ImageView
+
+lateinit var iv: ImageView
+var scale = 1f
+
+class MainActivity : AppCompatActivity() {
+
+    var ourSD: ScaleGestureDetector? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        iv = findViewById(R.id.idimg)
+        ourSD = ScaleGestureDetector(this, ScaleListener())
+        
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        ourSD!!.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    class ScaleListener: ScaleGestureDetector.SimpleOnScaleGestureListener(){
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            scale *= detector.scaleFactor
+            scale = Math.max(0.1f, Math.min(scale, 5.0f))
+            iv.scaleX = scale
+            iv.scaleY = scale
+
+            return super.onScale(detector)
+        }
+    }
+}
